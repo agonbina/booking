d6140a796af8b6819c842b44373919ffbe267c5f
Agon Bina <agon_bina@hotmail.com>
2015-01-26 18:05:28 +0100

    [feature] implemented the selected-room view

--- views/book-room/components/selected-room.js
+++ views/book-room/components/selected-room.js
@@ -4,7 +4,14 @@ module.exports = {
     computed: {
         room: function () {
             return this.$root.selectedRoom
+        },
+        avail: function () {
+            return this.room.avail
         }
+    },
+
+    components: {
+        'ui-timebar': require('../../search/components/timebar')
     },
 
     template: require('./selected-room.jade'),
