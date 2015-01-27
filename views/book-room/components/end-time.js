dd8ad78d85a58f4c43ef109a4349165b88ffdf3a
Agon Bina <agon_bina@hotmail.com>
2015-01-27 13:25:42 +0100

    [feature] end time availability updates when start time changes

--- views/book-room/components/end-time.js
+++ views/book-room/components/end-time.js
@@ -1,5 +1,7 @@
 
-var makeHMmixin = require('../mixins/makeHMmixin')
+var makeHMmixin = require('../mixins/makeHMmixin'),
+    moment = require('moment'),
+    not = require('101/not')
 
 module.exports = {
 
@@ -10,7 +12,7 @@ module.exports = {
             var hour = start.hour,
                 minute = start.minute
 
-            console.log(start)
+            vm.$set('start', start)
 
             if(!hour || !minute) {
                 vm.isDisabled = true
@@ -26,7 +28,9 @@ module.exports = {
         return {
             isDisabled: true,
             hour: '',
-            minute: ''
+            minute: '',
+            availability: { '7': [] },
+            start: {}
         }
     },
 
@@ -36,7 +40,54 @@ module.exports = {
                 this.$hour.dropdown('restore defaults')
                 this.$minute.dropdown('restore defaults')
             }
+        },
+
+        // Calculate the availability matrix when we have a new start time
+        'start.minute': function (min) {
+            var start = moment(this.start),
+                availability = this.$parent.availability.filter(function (interval) {
+                    var end = interval[1]
+                    return start.isBefore(end)
+                }),
+                vm = this
+
+            if(min) {
+                var begin = availability[0][0]
+
+                if(not(start.isSame(begin)) && start.isAfter(begin)) {
+                    availability[0][0] = start.add(15, 'm')
+                }
+
+                vm.availability = {}
+
+                availability.forEach(function (interval) {
+                    var next = interval[0],
+                        end = interval[1]
+
+                    while(next.isBefore(end) || next.isSame(end)) {
+                        var hour = parseInt(next.format('HH')),
+                            minutes = next.format('mm'),
+                            store = vm.availability[hour]
+
+                        if(store) {
+                            store.push(minutes)
+                        } else {
+                            vm.availability[hour] = [ minutes ]
+                        }
+
+                        next = next.add(15, 'm')
+                    }
+                })
+            }
         }
+    },
+
+    computed: {
+
+        minutes: function () {
+            return this.availability[this.hour]
+        }
+
     },
 
     template: require('./end-time.jade'),
